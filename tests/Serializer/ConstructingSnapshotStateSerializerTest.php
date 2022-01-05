f8d1e92c42d1951e5461cd12197fd2b84f971302
andreo <andrze.pakula@gmail.com>
2022-01-05 22:02:10 +0100

    Rename test function

--- tests/Serializer/ConstructingSnapshotStateSerializerTest.php
+++ tests/Serializer/ConstructingSnapshotStateSerializerTest.php
@@ -18,7 +18,7 @@ final class ConstructingSnapshotStateSerializerTest extends TestCase
     /**
      * @test
      */
-    public function state_serialized_and_unserialized(): void
+    public function state_serialized_and_deserialized(): void
     {
         $dummyState = new StateStub('foo', 'bar');
         $snapshotState = SnapshotState::from($dummyState);
